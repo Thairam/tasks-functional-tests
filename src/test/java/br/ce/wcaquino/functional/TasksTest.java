fc506194c73463aa2e88e136a4cc907e1cf2829b
thairam.ataide <thairam.ataide@cwi.com.br>
2024-08-12 22:16:14 -0300

    teste para remover tarefa

--- src/test/java/br/ce/wcaquino/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/functional/TasksTest.java
@@ -89,4 +89,27 @@ public class TasksTest {
       driver.quit();
     }
   }
+
+  @Test
+  public void deveRemoverTarefaComSucesso() throws MalformedURLException {
+    WebDriver driver = acessarAplicacao();
+    try {
+      // Inserir tarefa
+      driver.findElement(By.id("addTodo")).click();
+      driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+      driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
+
+      driver.findElement(By.id("saveButton")).click();
+
+      String message = driver.findElement(By.id("message")).getText();
+      Assert.assertEquals("Success!", message);
+
+      // Remover tarefa
+      driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
+      message = driver.findElement(By.id("message")).getText();
+      Assert.assertEquals("Success!", message);
+    } finally {
+      driver.quit();
+    }
+  }
 }
